55186651d9c12321e9fa88c96c5174eaafd9b82a
AdamGoris <adamgoris2@gmail.com>
2022-01-20 14:08:36 +1100

    Added x,y vars, draw rect method

--- src/include/letter.hpp
+++ src/include/letter.hpp
@@ -2,14 +2,33 @@
 
 class Letter
 {
+  private:
+    const int SIZE = 50;
+    int x;
+    int y;
+
   public:
     char value; // the character associated with this letter object
     bool value_is_visible; // whether to show the value in this letter
 
-    // Letter object constructor
-
-    Letter()
+    Letter(int x, int y) // Constructor
     {
       value_is_visible = false;
+      x = x;
+      y = y;
     }
+    
+    // Letter draw function
+
+    void draw(SDL_Renderer* renderer);
 };
+
+void Letter::draw(SDL_Renderer* renderer)
+{
+  SDL_Rect rect;
+  rect.x = x;
+  rect.y = y;
+  rect.w = SIZE;
+  rect.h = SIZE;
+  SDL_RenderDrawRect(renderer, &rect);
+}
