f533b7b3f3e54fec6a54f03a3d24779083a90bb5
AdamGoris <adamgoris2@gmail.com>
2022-01-20 16:43:36 +1100

    Moved x,y to public, default constructor, setPos()

--- src/include/letter.hpp
+++ src/include/letter.hpp
@@ -4,11 +4,11 @@ class Letter
 {
   private:
     const int SIZE = 100;
-    int x;
-    int y;
 
   public:
-    char value; // the character associated with this letter object
+    int x;
+    int y;
+    char value = '\0'; // the character associated with this letter object
     bool value_is_visible; // whether to show the value in this letter
 
     Letter(int x_pos, int y_pos)
@@ -18,15 +18,19 @@ class Letter
       y = y_pos;
     }
 
+    Letter() // Default constructor
+    {
+      value_is_visible = false;
+      x = 0;
+      y = 0;
+    }
+
     void draw(SDL_Renderer* renderer);
+    void setPos(int x_pos, int y_pos);
 };
 
 void Letter::draw(SDL_Renderer* renderer)
 {
-  // Set the draw color to black
-
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
   // Draw the outer rect of the letter
 
   SDL_Rect rect;
@@ -35,4 +39,10 @@ void Letter::draw(SDL_Renderer* renderer)
   rect.w = SIZE;
   rect.h = SIZE;
   SDL_RenderDrawRect(renderer, &rect);
+}
+
+void Letter::setPos(int x_pos, int y_pos)
+{
+  x = x_pos;
+  y = y_pos;
 }
