b667db231469969da52f9b3fb0371491f65bb55d
AdamGoris <adamgoris2@gmail.com>
2022-01-20 19:21:54 +1100

    Moved vars to private

--- src/include/letter.hpp
+++ src/include/letter.hpp
@@ -2,26 +2,27 @@
 
 class Letter
 {
-  public:
-    int x;
-    int y;
+  private:
+    int x_pos;
+    int y_pos;
     int size;
     char value = '\0'; // the character associated with this letter object
     bool value_is_visible; // whether to show the value in this letter
 
-    Letter(int x_pos, int y_pos, int s)
+  public:
+    Letter(int x, int y, int s)
     {
       value_is_visible = false;
-      x = x_pos;
-      y = y_pos;
+      x_pos = x;
+      y_pos = y;
       size = s;
     }
 
     Letter() // Default constructor
     {
       value_is_visible = false;
-      x = 0;
-      y = 0;
+      x_pos = 0;
+      y_pos = 0;
     }
 
     void draw(SDL_Renderer* renderer);
@@ -33,16 +34,16 @@ void Letter::draw(SDL_Renderer* renderer)
   // Draw the outer rect of the letter
 
   SDL_Rect rect;
-  rect.x = x;
-  rect.y = y;
+  rect.x = x_pos;
+  rect.y = y_pos;
   rect.w = size;
   rect.h = size;
   SDL_RenderDrawRect(renderer, &rect);
 }
 
-void Letter::setPos(int x_pos, int y_pos, int s)
+void Letter::setPos(int x, int y, int s)
 {
-  x = x_pos;
-  y = y_pos;
+  x_pos = x;
+  y_pos = y;
   size = s;
 }
