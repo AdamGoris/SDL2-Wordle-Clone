b070d053947d9f61a002e4162a808aae734ca502
AdamGoris <adamgoris2@gmail.com>
2022-01-28 10:46:46 +1100

    Removed constants, pass values to Row

--- src/include/grid.hpp
+++ src/include/grid.hpp
@@ -10,22 +10,28 @@ class Grid
   private:
     int x_pos;
     int y_pos;
+    int num_cols;
     int num_rows;
+    int letter_size;
+    int letter_gap;
 
   public:
     vector<Row> rows;
 
-    Grid(int x, int y, int nr)
+    Grid(int x, int y, int nc, int nr, int ls, int lg)
     {
       x_pos = x;
       y_pos = y;
+      num_cols = nc;
       num_rows = nr;
+      letter_size = ls;
+      letter_gap = ls + lg;
 
       // Initialise rows vector
 
       for (int i = 0; i < num_rows; i++)
       {
-        Row row(x_pos, y_pos, 100);
+        Row row(x_pos, y_pos, letter_size, letter_gap);
         rows.push_back(row);
 
         // Increment y pos for next row
