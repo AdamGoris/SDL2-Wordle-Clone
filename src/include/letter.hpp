9f8e70cc7a75bd33080b756d248195e88c738506
AdamGoris <adamgoris2@gmail.com>
2022-01-28 12:57:38 +1100

    Activate/Deactivate letter logic

--- src/include/letter.hpp
+++ src/include/letter.hpp
@@ -8,17 +8,21 @@ class Letter
     int size;
     char value = '\0'; // the character associated with this letter object
     bool value_is_visible; // whether to show the value in this letter
+    bool is_active; // Whether this is the active letter
 
   public:
-    Letter(int x, int y, int s)
+    Letter(int x, int y, int s, bool ia)
     {
       value_is_visible = false;
       x_pos = x;
       y_pos = y;
       size = s;
+      is_active = ia;
     }
 
     void draw(SDL_Renderer* renderer);
+    void activate();
+    void deactivate();
 };
 
 void Letter::draw(SDL_Renderer* renderer)
@@ -31,4 +35,26 @@ void Letter::draw(SDL_Renderer* renderer)
   rect.w = size;
   rect.h = size;
   SDL_RenderDrawRect(renderer, &rect);
+
+  // If this letter is active, draw another rect (for now)
+
+  if (is_active)
+  {
+    SDL_Rect a_rect;
+    a_rect.x = x_pos + 5;
+    a_rect.y = y_pos + 5;
+    a_rect.w = size - 10;
+    a_rect.h = size - 10;
+    SDL_RenderDrawRect(renderer, &a_rect);
+  }
+}
+
+void Letter::activate()
+{
+  is_active = true;
+}
+
+void Letter::deactivate()
+{
+  is_active = false;
 }
